a466a0a5f95ab43ffa6644bdf9bed17038acaa24
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-06-21 21:39:48 +0530

    for and while loop in list

---
+++ looplist.py
@@ -0,0 +1,13 @@
+thislist = ['akhilesh','yadav','manirampur','amethi']
+
+for x in thislist :
+    print(x)
+
+for i in range(len(thislist)):
+        print(thislist[i])
+
+i=0
+while i<len(thislist):
+     print(thislist[i])
+     i=i+1
+[print(x) for x in thislist]
