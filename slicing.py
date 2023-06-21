4b96012485578519f86041b769bae4119a4ad388
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-06-21 12:50:07 +0530

    modify string

--- slicing.py
+++ slicing.py
@@ -3,3 +3,17 @@ print(s[2:5])
 print(s[:5])
 print(s[2:])
 print(s[-5:-1])
+print(s.upper())
+name="AKHILESH yadav"
+print(name.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+name1="  Akhilesh  "
+
+print(name1.strip())
+
+print(name.replace("A","Z"))
+
+print(name.split(" "))
