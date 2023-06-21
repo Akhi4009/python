4307c8e1ded2064b0538b869687948cfab26d9ce
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-06-21 11:22:18 +0530

    comments

--- hello.py
+++ hello.py
@@ -1,1 +1,20 @@
 print('hello world')
+if 5 > 2:
+    print("Five is greater than two!")
+    print("Five is greater than two!")
+
+# this is variables
+
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+
+#This is a comment
+"""
+print("Hello, World!")
+print("Hello, World!")
+"""
+print("Hello, World!")
+print("Hello, World!") #This is a comment
