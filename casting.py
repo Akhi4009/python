7b5ca5e3d9f1ff9a023521a31f41c1f94ba186d1
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-06-21 11:36:59 +0530

    variables

---
+++ casting.py
@@ -0,0 +1,13 @@
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x,y,z)
+a = str(5)
+b = int(5)
+c = float(5)
+# print(a,b,c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+#Variable names are case-sensitive.
