d8f9e457e92e5387871c6a27b23377b9bceb47d8
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-06-21 11:57:04 +0530

    global and local variables

--- casting.py
+++ casting.py
@@ -26,6 +26,8 @@ MyVariableName = "John"
 
 my_variable_name = "John"
 
+#Python allows you to assign values to multiple variables in one line:
+#Make sure the number of variables matches the number of values, or else you will get an error.
 x, y, z, a = "Orange", "Banana", "Cherry", "Akhilesh"
 print(x)
 print(y)
@@ -33,4 +35,39 @@ print(z)
 print(a)
 
 
-                
+#And you can assign the same value to multiple variables in one line:
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+
+
+                #Unpack a Collection
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+def sum():
+  print(5 + 5)
+sum()
+
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
