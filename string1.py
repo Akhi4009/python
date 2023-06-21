b33baa8dedd64ff9a970f2fc5663f8441af98048
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-06-21 12:34:20 +0530

    string basic

---
+++ string1.py
@@ -0,0 +1,26 @@
+a = "Hello"
+print(a)
+
+#Multiline Strings
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+
+for x in "akhilesh" :
+    print(x)
+
+name="akhileshyadav"
+print(len(name))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+if "free" in txt :
+    print("hello from akhilesh")
+
+if "freefgf" not in txt:
+    print("hello from yadav")
