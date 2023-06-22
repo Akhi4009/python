bafc7fd4690373875f2b0a7819b48abb0d92f78f
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-06-22 08:27:18 +0530

    join list

---
+++ joinlist.py
@@ -0,0 +1,18 @@
+#concatenation method
+
+list1 = ["a","b","c"]
+list2 = [1,2,3]
+list3=list1 +list2
+print(list3)
+
+# append method
+
+for x in list2 : 
+    list1.append(x)
+
+
+# extend method
+
+list1.extend(list2)
+print(list1)
+
