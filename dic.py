573aaa9569c88ceea582da2f8f22832a06db1e40
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-06-22 10:09:13 +0530

    dic

---
+++ dic.py
@@ -0,0 +1,15 @@
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print(len(thisdict))
+
+thisdict1 = dict(name = "John", age = 36, country = "Norway")
+print(thisdict1)
+
+for x, y in thisdict.items():
+  print(x, y)
